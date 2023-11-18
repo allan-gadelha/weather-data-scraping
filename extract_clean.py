3ec0e93b7b88d3b9b2d8009d15942cb68cb3bb06
Allan Gadelha <c.allan.gadelha@gmail.com>
2023-11-18 12:03:29 -0300

    fix: improve data cleaning in extract_clean.py
    Refactored the `extract_clean.py` script to address issues with temperature and precipitation data. Implemented a new function to clean the temperature column, resolving errors caused by "\n" characters in the CSV. Additionally, created a new function, `clean_precipitation`, to handle cleaning for the "Precipitation/Rainfall mm (in)" column, which also contained "\n" characters.
    
    Now, the data is cleaned more effectively, providing accurate temperature and precipitation information in the CSV output files.

--- extract_clean.py
+++ extract_clean.py
@@ -1,69 +1,81 @@
-import pandas as pd  # Import the pandas library
+# Import the pandas library
+import pandas as pd  
 
 # Load the data from the CSV files
-df_sevilla = pd.read_csv('Seville.csv', header=None)  # Read the CSV file into a DataFrame
+# Read the CSV file into a DataFrame
+df_sevilla = pd.read_csv('Seville.csv', header=None)  
 
+# Function to clean data
 def clean_data(file_name, city_name):
     # Load the data from the CSV file
-    df = pd.read_csv(file_name, header=None)  # Read the CSV file into a DataFrame
+    # Read the CSV file into a DataFrame
+    df = pd.read_csv(file_name, header=None)  
 
     # Select the 'Min. Temperature ºC (ºF)', 'Max. Temperature ºC (ºF)', 'Precipitation / Rainfall mm (in)', 'avg. Sun hours (hours)', and 'Rainy days (d)' rows
-    df_clean = df[df[0].isin(['Min. Temperature °C (°F)', 'Max. Temperature °C (°F)', 'Precipitation / Rainfall mm (in)', 'avg. Sun hours (hours)', 'Rainy days (d)'])]  # Filter the DataFrame
+    # Filter the DataFrame
+    df_clean = df[df[0].isin(['Min. Temperature °C (°F)', 'Max. Temperature °C (°F)', 'Precipitation / Rainfall mm (in)', 'avg. Sun hours (hours)', 'Rainy days (d)'])]  
 
-    # Transpose the DataFrame
-    df_clean = df_clean.transpose()  # Transpose the DataFrame (swap rows and columns)
+    # Transpose the DataFrame (swap rows and columns)
+    df_clean = df_clean.transpose()  
 
-    # Set the first row as the header
-    df_clean.columns = df_clean.iloc[0]  # Set the first row as the column names
-    df_clean = df_clean[1:]  # Remove the first row
+    # Set the first row as the column names
+    df_clean.columns = df_clean.iloc[0]  
+    # Remove the first row
+    df_clean = df_clean[1:]  
 
-    # Reset the index
-    df_clean.reset_index(drop=True, inplace=True)  # Reset the index of the DataFrame
+    # Reset the index of the DataFrame
+    df_clean.reset_index(drop=True, inplace=True)  
 
-    # Add the city name to the DataFrame
-    df_clean['City'] = city_name  # Add a new column with the city name
+    # Add a new column with the city name
+    df_clean['City'] = city_name  
 
-    # Add the month names to the DataFrame
-    df_clean['Month'] = ['January', 'February', 'March', 'April', 'May', 'June', 'July', 'August', 'September', 'October', 'November', 'December']  # Add a new column with the month names
+    # Add a new column with the month names
+    df_clean['Month'] = ['January', 'February', 'March', 'April', 'May', 'June', 'July', 'August', 'September', 'October', 'November', 'December']  
 
-    # Rearrange the columns
-    df_clean = df_clean[['City', 'Month', 'Min. Temperature °C (°F)', 'Max. Temperature °C (°F)', 'Precipitation / Rainfall mm (in)', 'avg. Sun hours (hours)', 'Rainy days (d)']]  # Rearrange the columns of the DataFrame
+    # Rearrange the columns of the DataFrame
+    df_clean = df_clean[['City', 'Month', 'Min. Temperature °C (°F)', 'Max. Temperature °C (°F)', 'Precipitation / Rainfall mm (in)', 'avg. Sun hours (hours)', 'Rainy days (d)']]  
 
-    return df_clean  # Return the cleaned DataFrame
+    # Return the cleaned DataFrame
+    return df_clean  
 
-# Call the function for each CSV file
-df_sevilla = clean_data('Seville.csv', 'Seville')  # Clean the data for Seville
+# Clean the data for Seville
+df_sevilla = clean_data('Seville.csv', 'Seville')  
 
-# Concatenate the DataFrames
-df = pd.concat([df_sevilla])  # Concatenate the DataFrames into a single DataFrame
+# Concatenate the DataFrames into a single DataFrame
+df = pd.concat([df_sevilla])  
 
-# Replace the '\n' in the 'Avg. Temperature °C (°F)' column with ' ('
-df['Min. Temperature °C (°F)'] = df['Min. Temperature °C (°F)'].str.replace('\n', ' (')  # Replace newline characters with ' ('
-df['Max. Temperature °C (°F)'] = df['Max. Temperature °C (°F)'].str.replace('\n', ' (')  # Replace newline characters with ' ('
+# Function to clean temperature
+def clean_temperature(df, column):
+    # Replace the '\n' in the column with ' ('
+    df[column] = df[column].str.replace('\n', ' (')
+    
+    # Add ')' to the end of the column
+    df[column] += ')'
+    
+    # Extract the numeric values from the column
+    df[column] = df[column].str.extract(r'\(([0-9\.]*)\) °F', expand=False)
+    
+    return df
 
-# Add ')' to the end of the 'Avg. Temperature °C (°F)' column
-df['Min. Temperature °C (°F)'] = df['Min. Temperature °C (°F)'] + ')'  # Add ')' to the end of the string
-df['Max. Temperature °C (°F)'] = df['Max. Temperature °C (°F)'] + ')'  # Add ')' to the end of the string
+# Columns to clean
+columns_to_clean = ['Min. Temperature °C (°F)', 'Max. Temperature °C (°F)']
 
-# Extract the Fahrenheit values from the 'Min. Temperature °C (°F)' and 'Max. Temperature °C (°F)' columns
-df['Min. Temperature °C (°F)'] = df['Min. Temperature °C (°F)'].str.extract(r'\((.*?) °F\)', expand=False)
-df['Max. Temperature °C (°F)'] = df['Max. Temperature °C (°F)'].str.extract(r'\((.*?) °F\)', expand=False)
+# Clean temperature for each column
+for column in columns_to_clean:
+    df = clean_temperature(df, column)
 
-# Remove the parentheses from the 'Min. Temperature °C (°F)' and 'Max. Temperature °C (°F)' columns
-df['Min. Temperature °C (°F)'] = df['Min. Temperature °C (°F)'].str.replace('(', '').str.replace(')', '') + ' °F'
-df['Max. Temperature °C (°F)'] = df['Max. Temperature °C (°F)'].str.replace('(', '').str.replace(')', '') + ' °F'
+# Function to clean precipitation
+def clean_precipitation(df, column):
+    # Replace the '\n' in the column with ' '
+    df[column] = df[column].str.replace('\n', ' ')
+    return df
 
-# Remove 'F' from the 'Min. Temperature °C (°F)' and 'Max. Temperature °C (°F)' columns
-df['Min. Temperature °C (°F)'] = df['Min. Temperature °C (°F)'].str.replace('F', '')
-df['Max. Temperature °C (°F)'] = df['Max. Temperature °C (°F)'].str.replace('F', '')
-
-# Remove 'º' from the 'Min. Temperature °C (°F)' and 'Max. Temperature °C (°F)' columns
-df['Min. Temperature °C (°F)'] = df['Min. Temperature °C (°F)'].str.replace('°', '')
-df['Max. Temperature °C (°F)'] = df['Max. Temperature °C (°F)'].str.replace('°', '')
+# Clean precipitation
+df = clean_precipitation(df, 'Precipitation / Rainfall mm (in)')
 
 # Print the final DataFrame
 print(df)
 
 # Save the DataFrame to a CSV file with ';' as the delimiter
-#Econding ISO-8859-1 because some countries have accent in some words: "ó" "ô"
+# Encoding ISO-8859-1 because some countries have accent in some words: "ó" "ô"
 df.to_csv('Seville_Clean.csv', index=False, encoding='ISO-8859-1', sep=';')
